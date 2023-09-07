959bfb65501d7947a56fd34b8f19c8f7fdae2cef
Sakindeb <sakindeborah@gmail.com>
2023-09-07 14:20:19 +0300

    Added requirements file and hosted on python anywhere

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ def get_info():
     # Get the current day of the week
     current_day = datetime.datetime.now().strftime("%A")
 
-    # Get the current UTC time with validation of +/-2 minutes
+    # Get the current UTC time
     utc_time = datetime.datetime.now(pytz.UTC)
     utc_time_str = utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    # Construct GitHub URLs based on your repository and file names
-    github_repo_url = "https://github.com/username/repo"
-    github_file_url = f"{github_repo_url}/blob/main/file_name.ext"
+    # GitHub URLs 
+    github_repo_url = "https://github.com/Sakindeb/Backend"
+    github_file_url = "https://github.com/Sakindeb/Backend/blob/main/app.py"
 
     # Create the response JSON
     response = {
